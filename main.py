b8bad33ce6149f3a049a31f879d7d3310f946929
kulvirvirk <kvirkdev@gmail.com>
2020-02-21 23:58:25 +0000

    added code example of iteration over string object

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# ========================================== #
+# 1. using a for loop iterate over a string  #
+# 2. using a for loop iterate over a list    #
+# ========================================== #
+
 # 1. using a for loop iterate over a string
-# 2. using a for loop iterate over a list
- 
+some_string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+for specific_string_value in some_string:
+  print(specific_string_value)
