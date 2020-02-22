6d6a7010f45e4cf0e6a55f93e6e2f5d958e5ac19
kulvirvirk <kvirkdev@gmail.com>
2020-02-22 00:01:41 +0000

    added code example of iteration over list object

--- main.py
+++ main.py
@@ -4,6 +4,15 @@
 # ========================================== #
 
 # 1. using a for loop iterate over a string
-some_string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+some_string = 'Lorem ipsum dolor sit amet'
 for specific_string_value in some_string:
   print(specific_string_value)
+print('--------------******--------------\n')
+
+# 2. using a for loop iterate over a list 
+some_list = ['kiwi', 'mango', 'apple', 'watermelon', 'papaya']
+for specific_list_value in some_list: 
+  print(specific_list_value)
+print('--------------******--------------\n')
+
+
